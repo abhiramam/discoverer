f77703b22e022cf6260f0bd8ccd2e333e97a8dc6
Abhiram Ambadipudi <abhiramambadipudi4444@gmail.com>
2024-01-22 21:36:32 +0530

    Initial commit

---
+++ src/test/java/org/scaler/discoverer/DiscovererApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scaler.discoverer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscovererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
